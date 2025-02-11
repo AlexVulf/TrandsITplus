c274acf7544f7916891a88c46a0d64d3acd6a6e4
Alex S. Vulf <101990036+AlexVulf@users.noreply.github.com>
2025-02-10 22:31:29 -0300

    atualização

--- 1973.py
+++ 1973.py
@@ -1,27 +1,28 @@
+import sys
+
 def jornada_nas_estrelas(N, carneiros):
-    visitados = set()
+    visitados = [False] * N
     i = 0
     
     while 0 <= i < N:
-        visitados.add(i)
+        visitados[i] = True
         
         if carneiros[i] == 0:
             break
-        
-        carneiros[i] -= 1 
-        
-        if carneiros[i] % 2 == 0:
-            i -= 1
-        else:
-            i += 1
 
-    estrelas_atacadas = len(visitados)
+        direcao = 1 if carneiros[i] % 2 == 1 else -1
+
+        carneiros[i] -= 1
+
+        i += direcao
+
+    estrelas_atacadas = sum(visitados)
     carneiros_restantes = sum(carneiros)
 
     return estrelas_atacadas, carneiros_restantes
 
-N = int(input())
-carneiros = list(map(int, input().split()))
+N = int(sys.stdin.readline().strip())  
+carneiros = list(map(int, sys.stdin.readline().strip().split()))  
 
 resultado = jornada_nas_estrelas(N, carneiros)
 print(resultado[0], resultado[1])
